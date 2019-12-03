be2b764742eecbaa93c97683a5e82a09130375af
Crist <crisaturn8@gmail.com>
2019-12-03 10:48:25 +0100

    new page connect

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,10 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( util.css )
+Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += %w( animate.css )
+Rails.application.config.assets.precompile += %w( daterangepicker.css )
+Rails.application.config.assets.precompile += %w( animsition.min.css )
+Rails.application.config.assets.precompile += %w( hamburgers.min.css )
+Rails.application.config.assets.precompile += %w( select2.min.css )
